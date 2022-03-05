0208840250cb2081bf7446c30c422239dd24d2a0
shaneashford <96359059+shaneashford@users.noreply.github.com>
2022-03-05 15:24:09 -0500

    Create madlibs.py

---
+++ madlibs.py
@@ -0,0 +1,9 @@
+# A simple beginner program to demonstrate understanding of prompting for and storing input, f-strings, variables, concatenation, and printing.
+
+noun = input('Type a noun ')
+adjective = input('Type an adjective ')
+verb = input('Type a verb ')
+
+madlib = f'The {adjective} {noun} {verb}'
+
+print(madlib)
